609c68ef122ce8f8d1fd5435af594351926d36d3
fungleo <web@fengcms.com>
2025-05-24 22:57:59 +0800

    更新视频工具内容

--- src/comps/VideoTool.vue
+++ src/comps/VideoTool.vue
@@ -8,8 +8,8 @@
       append-to-body
     >
       <div class="video-tool-content">
-        <Tip block>1. 首先去优酷或其他视频网站，获取视频通用分享代码</Tip>
-        <img src="/image/youku-help.jpg">
+        <Tip block>1. 首先去 bilibili 视频网站，获取视频嵌入分享代码</Tip>
+        <img width="550" src="/image/youku-help.jpg">
         <Tip block>2. 将复制过来代码，粘贴于下方文本框</Tip>
         <el-input v-model="videoCode" type="textarea" @change="filterCode" />
         <Tip block>3. 点击下方“获取视频地址”按钮，即可完成。</Tip>
@@ -28,33 +28,38 @@ export default {
   props: {
     value: { type: String, default: '' }
   },
-  data () {
+  data() {
     return {
       isShowBox: false,
       videoCode: ''
     }
   },
   methods: {
-    initVideoTool () {
+    initVideoTool() {
       this.isShowBox = true
     },
-    filterCode (str) {
+    filterCode(str) {
       this.videoCode = str.replaceAll('<', '').replaceAll('>', '')
     },
-    onSubmit () {
+    onSubmit() {
       try {
-        const str = this.videoCode.split(' ').find(i => i.includes('src=')).split('src=').pop() || ''
-        const url = str.replaceAll('\'', '').replaceAll('\"', '')
+        const str =
+          this.videoCode
+            .split(' ')
+            .find((i) => i.includes('src='))
+            .split('src=')
+            .pop() || ''
+        const url = str.replaceAll("'", '').replaceAll('"', '')
         const realUrl = url.substring(0, 2) === '//' ? `https:${url}` : url
         if (isExternal(realUrl)) {
           this.$emit('input', realUrl)
           this.isShowBox = false
-          this.$message.success(`获取视频地址成功`)
+          this.$message.success('获取视频地址成功')
         } else {
-          this.$message.error(`获取视频地址失败，请检查通用代码是否正确`)
+          this.$message.error('获取视频地址失败，请检查通用代码是否正确')
         }
       } catch (e) {
-        this.$message.error(`获取视频地址失败，请检查通用代码是否正确`)
+        this.$message.error('获取视频地址失败，请检查通用代码是否正确')
       }
     }
   }
